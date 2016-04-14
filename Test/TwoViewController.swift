5d95040ef271c38f11308f03eb09bcf6c4f55975
MOXZ <ktmswzw@gmail.com>
2016-04-14 09:13:05 +0800

    init

---
+++ Test/TwoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TwoViewController.swift
+//  Test
+//
+//  Created by Vincent on 4/14/16.
+//  Copyright © 2016 Vincent. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func prepareForUnwind2(segue: UIStoryboardSegue){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
